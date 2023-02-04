8715e9314c2db5efab6ff8abf105dc00151c6343
0xc000007b <rammleid123@gmail.com>
2023-02-04 17:44:04 +0300

    first commit

---
+++ noobsite/todo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=400, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False, null=False)
+
+    def __str__(self):
+        return self.title
+
